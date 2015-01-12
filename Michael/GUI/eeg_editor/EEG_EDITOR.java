9482e465876c6185e3ac51cb766a4bf8de1fb73c
Michael <michael.brochonski@gmail.com>
2015-01-12 11:27:13 -0200

    EEG_Editor_GUI
    Interface Gráfica com Binders e Eventos prontos para serem adicionados

---
+++ Michael/GUI/eeg_editor/EEG_EDITOR.java
@@ -0,0 +1,16 @@
+package eeg_editor;
+
+/**
+ *
+ * @author Michael
+ */
+public class EEG_EDITOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
